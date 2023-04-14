078e1efe55223004cd124fc0ab8961a3ed131a12
gozooit <gozoo.it@protonmail.com>
2023-04-14 17:03:25 +0200

    add more Game logic
    - logic to play a turn
    - logic to print players details

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,13 @@ module TicTacToe
       row[column] = symbol
     end
 
+    def full?
+      [@first_row, @second_row, @third_row].each do |row|
+        return false if row.any?(' ')
+      end
+      true
+    end
+
     private
 
     # return a row corresponding to the input position (a, b or c)
@@ -55,26 +62,52 @@ module TicTacToe
     def initialize
       initialize_players
       @board = Board.new
+      @current_player = [true, false].sample ? @player1 : @player2
     end
 
-    # def initialize_players_symbols
-    #   symbols = %w[X O]
-    #   @player1.symbol = symbols.delete_at(rand(2))
-    #   @player2.symbol = symbols.join
-    #   puts "#{@player1.name} is playing '#{@player1.symbol}'."
-    #   puts "#{@player2.name} is playing '#{@player2.symbol}'."
-    # end
+    def players_details
+      puts "Player 1 name : #{@player1.name}"
+      puts "Player 1 symbol : #{@player1.symbol}"
+      puts
+      puts "Player 2 name : #{@player2.name}"
+      puts "Player 2 symbol : #{@player2.symbol}"
+    end
 
-    # def switch_players_symbols
-    #   tmp = @player1.symbol
-    #   @player1.symbol = @player2.symbol
-    #   @player2.symbol = tmp
-    #   puts "#{@player1.name} is playing '#{@player1.symbol}'."
-    #   puts "#{@player2.name} is playing '#{@player2.symbol}'."
-    # end
+    def newgame_presentation
+      puts 'The Tic Tac Toe game begins !'
+      puts
+      players_details
+      puts
+      @board.display
+      puts
+      puts "First player to play is : #{@current_player.name}"
+      puts
+    end
 
-    def play_turn
+    def play
+      newgame_presentation
+      until @board.full? # || win?
+        play_turn
+        @current_player == @player1 ? @player2 : @player1
+      end
+    end
 
+    def play_turn(current_player = @current_player)
+      puts "#{current_player.name} it is your turn to play, chose an emplacement (A1 to C3)"
+      position = gets.chomp
+      while true
+        begin
+          add_symbol(position, current_player.symbol)
+        rescue StandardError
+          puts "#{position} is not a valid emplacement."
+          puts "Please choose a different position (A1 to C3)"
+          position = gets.chomp
+        else
+          puts
+          @board.display
+          puts
+        end
+      end
     end
 
     private
